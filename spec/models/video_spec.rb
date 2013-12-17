d59e78d506ec388a533df4f4771c41c9c347304b
sammy <sammy>
2013-12-17 22:40:30 +0100

    watched first bit of solution and realised i hadnt implemented quite a few cases

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,14 +6,29 @@ describe Video do
   it { should validate_presence_of(:description) }
 
   describe ".search_by_title" do
-    it "returns an array of matching results ordered by title" do
-      Video.create(title: "Ghostbusters", description: "80's funny movie")
-      Video.create(title: "I heart Huckabees", description: "Existential detectives at work")
-      Video.create(title: "Ghost Rider", description: "Some description")
-      search_term = "ghost"
-      expect(Video.search_by_title(search_term)).to eq [Video.last, Video.first] 
-      expect(Video.search_by_title(search_term)).to_not include Video.find(2)
+    Video.create(title: "Ghostbusters", description: "80's funny movie")
+    Video.create(title: "I heart Huckabees", description: "Existential detectives at work")
+    Video.create(title: "Ghost Rider", description: "Some description")
+    match_exact = "Huckabees"
+    match_many = "ghost"
+    match_none = "Soutpark"
+    match_one_partial = "bust"
 
+    it "returns an empty array if there is no match" do
+      expect(Video.search_by_title(match_none)).to eq []
+    end
+
+    it "returns an array of one video if there is an exact match" do
+      expect(Video.search_by_title(match_exact)).to eq [Video.find(2)]
+    end
+
+    it "returns an array of one video if there is a partial match" do
+      expect(Video.search_by_title(match_one_partial)).to eq [Video.find(1)]
+    end
+
+    it "returns an array of matching results ordered by created_at" do
+      expect(Video.search_by_title(match_many)).to eq [Video.last, Video.first] 
+      expect(Video.search_by_title(match_many)).to_not include Video.find(2)
     end 
   end
 
