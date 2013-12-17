3f5a4a26851f526c576e1c17250971bf8a7f1ba6
Apostolos Samatas <flouts@gmail.com>
2013-12-17 15:38:21 +0100

    Changed spec file

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Video do 
   it "saves itself" do
-    video = Video.new(title: "Ghostbusters", description: "80's movie", small_cover_url: "ghost.jpg", large_cover_url: "ghost_large.jpg")
+    video = Video.new(title: "Ghostbusters", description: "80's movie", small_cover_url: "ghostbusters.jpg", large_cover_url: "ghostbusters_large.jpg")
     video.save
     Video.first.title.should == "Ghostbusters"
   end
