e6117c46c1c3ad3fe6d309ca99ca45b0d0deff35
sammy <sammy>
2013-12-17 23:21:39 +0100

    finalized specs for search_by_title

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,30 +6,31 @@ describe Video do
   it { should validate_presence_of(:description) }
 
   describe ".search_by_title" do
-    Video.create(title: "Ghostbusters", description: "80's funny movie")
-    Video.create(title: "I heart Huckabees", description: "Existential detectives at work")
-    Video.create(title: "Ghost Rider", description: "Some description")
-    match_exact = "Huckabees"
-    match_many = "ghost"
-    match_none = "Soutpark"
-    match_one_partial = "bust"
+    
+    let(:ghostbusters) {Video.create(title: "Ghostbusters", description: "80's funny movie", created_at: 1.day.ago)}
+    let(:huckabees) {Video.create(title: "I heart Huckabees", description: "Existential detectives at work", created_at: 2.days.ago)}
+    let(:ghost_rider) {Video.create(title: "Ghost Rider", description: "Some description", created_at: 2.minutes.ago)}
 
     it "returns an empty array if there is no match" do
-      expect(Video.search_by_title(match_none)).to eq []
+      expect(Video.search_by_title("no-match")).to eq []
     end
 
     it "returns an array of one video if there is an exact match" do
-      expect(Video.search_by_title(match_exact)).to eq [Video.find(2)]
+      expect(Video.search_by_title("Huckabees")).to eq [huckabees]
     end
 
     it "returns an array of one video if there is a partial match" do
-      expect(Video.search_by_title(match_one_partial)).to eq [Video.find(1)]
+      expect(Video.search_by_title("bust")).to eq [ghostbusters]
     end
 
     it "returns an array of matching results ordered by created_at" do
-      expect(Video.search_by_title(match_many)).to eq [Video.last, Video.first] 
-      expect(Video.search_by_title(match_many)).to_not include Video.find(2)
+      expect(Video.search_by_title("ghost")).to eq [ghost_rider,ghostbusters] 
+      expect(Video.search_by_title("ghost")).to_not include huckabees
     end 
+
+    it "returns an empty array if the search term is an empty string" do
+      expect(Video.search_by_title("")).to eq []
+    end
   end
 
 end
