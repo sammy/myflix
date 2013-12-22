cb3ea90113ab53347fe4d05cb31ba8361ada848a
sammy <sammy>
2013-12-22 23:24:33 +0100

    Implemented search functionality in the UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Myflix::Application.routes.draw do
   get 'home', to: "videos#home"
 
 
-  resources :videos, only: [:show]
+  resources :videos, only: [:show] do
+    collection do
+      post :search
+    end
+  end
+
   resources :categories, only: [:show]
+
 end
