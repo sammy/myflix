2acc11523d20fda3f6749feea108b48e5dfb67ac
Sammy <flouts@gmail.com>
2014-01-19 22:28:33 +0100

    completed authentication

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Myflix::Application.routes.draw do
 
   get 'register', to: "users#new"
   get 'sign_in', to: "sessions#new"
+  get 'sign_out', to: "sessions#destroy"
+
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
 
 end
