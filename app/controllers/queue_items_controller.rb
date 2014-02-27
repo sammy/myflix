c5d13101eac35aaeac1e0c658f20eb892a450f7d
Sammy <flouts@gmail.com>
2014-02-27 12:13:41 +0100

    removed comment

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -19,7 +19,6 @@ class QueueItemsController < ApplicationController
   def destroy
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy unless queue_item.user != current_user
-    # queue_item.destroy
     redirect_to my_queue_path
   end
 
