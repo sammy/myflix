14c0e9c29b9879f2bbad5c571f83066d351cadec
Sammy <flouts@gmail.com>
2014-03-30 23:35:32 +0200

    fixed issue with ratings in my_queue not updating properly

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -13,9 +13,10 @@ class QueueItem < ActiveRecord::Base
   end
 
   def rating=(new_rating)
-    if review && new_rating != nil
-      review.update_attributes(rating: new_rating)
-    elsif review && new_rating == nil
+    if review && new_rating.to_i.between?(1,5)
+      review.attributes = {rating: new_rating}
+      review.save(validate: false)
+    elsif review && new_rating.blank?
       review.destroy
     elsif review.nil?
       review = Review.new(user_id: user.id, video_id: video.id, rating: new_rating)
