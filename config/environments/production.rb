8e65f92f62b173cbf7bccc0474b8ea554424bab6
sammy <sammy>
2014-04-12 19:00:23 +0200

    fixed error in smtp configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Myflix::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    username:             ENV['GMAIL_USERNAME'],
+    user_name:             ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true 
