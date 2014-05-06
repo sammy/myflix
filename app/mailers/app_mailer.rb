317ee4cc3e6a26fb1608d7d195f73ee472bd7c27
sammy <sammy>
2014-05-06 23:20:59 +0200

    changed app mailer for staging

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,16 +2,22 @@ class AppMailer < ActionMailer::Base
 
   def sign_in_notification(user)
     @user = user
-    mail from: 'no-reply@myflix.com', to: @user.email, subject: 'Welcome to Myflix!'
+    mail from: 'no-reply@myflix.com', to: recipient(@user.email), subject: 'Welcome to Myflix!'
   end
 
   def reset_password_notification(user)
     @user = user
-    mail from: 'no-reply@myflix.com', to: @user.email, subject: 'Password reset'
+    mail from: 'no-reply@myflix.com', to: recipient(@user.email), subject: 'Password reset'
   end
 
   def invite_user(invitation)
     @invitation = invitation
-    mail from: 'no-reply@myflix.com', to: @invitation.recipient_email , subject: "#{@invitation.inviter.full_name} has invited you to MyFlix!"
+    mail from: 'no-reply@myflix.com', to: recipient(@invitation.recipient_email) , subject: "#{@invitation.inviter.full_name} has invited you to MyFlix!"
+  end
+
+  private
+
+  def recipient(user)
+    Rails.env.production? ? user : 'flouts@gmail.com'
   end
 end
