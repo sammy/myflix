75992e21f05f40c462949d2bfc5c51b71030bf02
sammy <flouts@gmail.com>
2014-05-14 00:01:18 +0200

    Update fog.rb

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJCGKRFX6BRQKNL5Q',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => 'access_key',                        # required
+    :aws_secret_access_key  => 'secret_access_key',                        # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     # :endpoint               => 'http://bckt.full.of.sand.s3.amazonaws.com', # optional, defaults to nil
     # :host                   => 's3.amazonaws.com'
